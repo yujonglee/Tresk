8a2f5e266313a1b0cdad103b70cb7501255e0268
yujong-lee <yujonglee@g.skku.edu>
2021-07-01 20:55:31 +0900

    의도 잘 드러나도록 Task 테스트를 개선하라

--- src/components/Task.test.tsx
+++ src/components/Task.test.tsx
@@ -3,86 +3,35 @@
  */
 
 import { useDispatch, useSelector } from 'react-redux';
-import { fireEvent, render, RenderResult } from '@testing-library/react';
-import given from 'given2';
+import { render } from '@testing-library/react';
 
-import { updateSelectedTaskId } from '../redux_module/todoSlice';
 import Task from './Task';
 
 describe('Task', () => {
   const dispatch = jest.fn();
-
-  const renderTask = (id: number): RenderResult => render((
-    <Task id={id} />
-  ));
 
   beforeEach(() => {
     dispatch.mockClear();
     (useDispatch as jest.Mock).mockReturnValue(dispatch);
 
     (useSelector as jest.Mock).mockImplementation((selector) => selector({
-      todo: { tasks: given.tasks },
+      todo: {
+        tasks: {
+          0: { title: 'root', subTasks: [1, 2], isOpen: true },
+          1: { title: 'task1', subTasks: [], isOpen: true },
+          2: { title: 'task2', subTasks: [3], isOpen: true },
+          3: { title: 'task3', subTasks: [4], isOpen: true },
+          4: { title: 'task4', subTasks: [], isOpen: true },
+        },
+      },
     }));
   });
 
-  describe('title', () => {
-    given('tasks', () => ({
-      1: { title: 'task1', subTasks: [] },
-    }));
+  it('renders tasks recursively', () => {
+    const { getByText } = render(<Task id={0} />);
 
-    it('is rendered', () => {
-      const { container } = renderTask(1);
-
-      expect(container).toHaveTextContent('task1');
-    });
-
-    context('when title is cilcked', () => {
-      it('updates selectedTaskId', () => {
-        const { getByText } = renderTask(1);
-        fireEvent.click(getByText('task1'));
-
-        expect(dispatch).toBeCalledWith(updateSelectedTaskId(1));
-      });
-    });
-  });
-
-  context('when id is 0', () => {
-    given('tasks', () => ({
-      0: { title: 'root', subTasks: [] },
-    }));
-
-    it('is not rendered', () => {
-      const { queryByText } = renderTask(0);
-
-      expect(queryByText('root')).not.toBeInTheDocument();
-    });
-  });
-
-  context('when subTasks is opened', () => {
-    given('tasks', () => ({
-      1: { title: 'task1', subTasks: [2], isOpen: true },
-      2: { title: 'task2', subTasks: [], isOpen: true },
-    }));
-
-    it('renders subTask title', () => {
-      const { container } = renderTask(1);
-
-      expect(container).toHaveTextContent('task1');
-      expect(container).toHaveTextContent('task2');
-    });
-  });
-
-  context('when subTasks is not opened', () => {
-    given('tasks', () => ({
-      1: { title: 'task1', subTasks: [2], isOpen: false },
-      2: { title: 'task2', subTasks: [], isOpen: false },
-    }));
-
-    it("doesn't renders subTask title", () => {
-      const { container } = renderTask(1);
-
-      expect(container).toHaveTextContent('task1');
-      expect(container).not.toHaveTextContent('task2');
+    ['task1', 'task2', 'task3', 'task4'].forEach((title) => {
+      expect(getByText(title)).toBeInTheDocument();
     });
   });
 });
