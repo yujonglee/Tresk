9a9418d5f0cd39da376f1227495ec663d1e025d5
yujong-lee <yujonglee@g.skku.edu>
2021-07-01 13:11:40 +0900

    e2e테스트를 fold와 unfold로 나눠라

--- test/unfold_test.js
+++ test/unfold_test.js
@@ -1,6 +1,6 @@
-Feature('Fold');
+Feature('Unfold');
 
-Scenario('can fold tasks', ({ I }) => {
+Scenario('can unfold tasks', ({ I }) => {
     I.amOnPage('http://localhost:8080');
 
     I.fillField('할 일', '첫 번째 할 일');
@@ -15,8 +15,12 @@ Scenario('can fold tasks', ({ I }) => {
     I.click('추가');
 
     I.click('//*[@id="app"]/div/div[1]/ul/li/ul/li/button[2]');
+    I.click('//*[@id="app"]/div/div[1]/ul/li/button[2]');
+
+    I.click('펼치기');
+    I.see('두 번째 할 일');
     I.dontSee('세 번째 할 일');
 
-    I.click('//*[@id="app"]/div/div[1]/ul/li/button[2]');
-    I.dontSee('두 번째 할 일');
+    I.click('펼치기');
+    I.see('세 번째 할 일');
 });
