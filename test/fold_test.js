6948bac77779212e351c07c532e6b78ccede44fa
yujong-lee <yujonglee@g.skku.edu>
2021-07-02 09:42:59 +0900

    unfold fold e2e 테스트파일을 하나로 합쳐라

--- test/fold_test.js
+++ test/fold_test.js
@@ -1,4 +1,4 @@
-Feature('Fold');
+Feature('Fold/Unfold');
 
 Scenario('can fold tasks', ({ I }) => {
   I.amOnPage('http://localhost:8080');
@@ -19,4 +19,29 @@ Scenario('can fold tasks', ({ I }) => {
 
   I.click('//*[@id="app"]/div/div[1]/ul/li/button[2]');
   I.dontSee('두 번째 할 일');
+});
+
+Scenario('can unfold tasks', ({ I }) => {
+  I.amOnPage('http://localhost:8080');
+
+  I.fillField('할 일', '첫 번째 할 일');
+  I.click('추가');
+
+  I.click('첫 번째 할 일');
+  I.fillField('할 일', '두 번째 할 일');
+  I.click('추가');
+
+  I.click('두 번째 할 일');
+  I.fillField('할 일', '세 번째 할 일');
+  I.click('추가');
+
+  I.click('//*[@id="app"]/div/div[1]/ul/li/ul/li/button[2]');
+  I.click('//*[@id="app"]/div/div[1]/ul/li/button[2]');
+
+  I.click('펼치기');
+  I.see('두 번째 할 일');
+  I.dontSee('세 번째 할 일');
+
+  I.click('펼치기');
+  I.see('세 번째 할 일');
 });
