ba16f089928c8eaa00309082ec87b3b0e849f969
yujong-lee <yujonglee@g.skku.edu>
2021-07-05 13:17:44 +0900

    e2e를 수정하라

--- test/restore_test.js
+++ test/restore_test.js
@@ -10,15 +10,19 @@ Scenario('Can restore deleted tasks', ({ I }) => {
   I.fillField('할 일', '두 번째 할 일');
   I.click('추가');
 
-  I.click('//*[@id="app"]/div/div[2]/ul/li[1]/button[2]');
-  I.click('완료');
+  I.click('//*[@id="app"]/div/div[2]/ul/li[2]/button[2]');
+  I.click('//*[@id="app"]/div/div[2]/ul/li/button[2]');
 
   I.click('로그 열기');
+  I.see('# 첫 번째 할 일');
+  I.see('# 두 번째 할 일');
+  I.click('로그 닫기');
+
+  I.click('되돌리기');
+  I.see('첫 번째 할 일');
+  I.dontSee('두 번째 할 일');
+
+  I.click('되돌리기');
   I.see('첫 번째 할 일');
   I.see('두 번째 할 일');
-
-  I.click('//*[@id="app"]/div/div[3]/ul/div[2]/li/button');
-  I.click('로그 닫기');
-  I.see('두 번째 할 일');
-  I.dontSee('첫 번째 할 일');
 });
