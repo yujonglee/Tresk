dcac6f2d715e0df29a72f773eae325bce88296f2
yujong-lee <yujonglee@g.skku.edu>
2021-07-07 13:19:12 +0900

    전체 페이지에에 Fade-in효과를 적용하고, TreeCanvas의 배경색을 조정하라

--- src/Page/Main.jsx
+++ src/Page/Main.jsx
@@ -1,4 +1,6 @@
 import { Grid, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import FadeIn from 'react-fade-in';
 
 import Header from '../components/Header';
 import TitleFieldContainer from '../components/TitleFieldContainer';
@@ -6,28 +8,41 @@ import RestoreTaskButton from '../components/RestoreTaskButton';
 import LogBookControlContainer from '../components/LogBookControlContainer';
 import Content from '../components/Content';
 
+const useStyles = makeStyles({
+  root: {
+    margin: '100px auto',
+  },
+});
+
 export default function Main() {
+  const classes = useStyles();
+
   return (
-    <Container maxWidth="md">
-      <Grid
-        container
-        direction="row"
-        justify="center"
-        alignItems="flex-start"
-        spaceing={6}
+    <FadeIn delay={300}>
+      <Container
+        className={classes.root}
+        maxWidth="md"
       >
-        <Grid item lg={12} md={12} sm={12} xs={12}>
-          <Header />
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="flex-start"
+          spaceing={6}
+        >
+          <Grid item lg={12} md={12} sm={12} xs={12}>
+            <Header />
+          </Grid>
+          <Grid item lg={12} md={12} sm={12} xs={12}>
+            <TitleFieldContainer />
+            <RestoreTaskButton />
+            <LogBookControlContainer />
+          </Grid>
+          <Grid item lg={12} md={12} sm={12} xs={12}>
+            <Content />
+          </Grid>
         </Grid>
-        <Grid item lg={12} md={12} sm={12} xs={12}>
-          <TitleFieldContainer />
-          <RestoreTaskButton />
-          <LogBookControlContainer />
-        </Grid>
-        <Grid item lg={12} md={12} sm={12} xs={12}>
-          <Content />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </FadeIn>
   );
 }
