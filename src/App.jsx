16a28a8489d55fe4b1fc1ce3d61aff4be35fe828
yujong-lee <yujonglee@g.skku.edu>
2021-07-17 14:51:35 +0900

    키보드 좌우방향키로 선택한 테스크의 깊이를 바꿀 수 있는 기능을 추가하라

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Route, Switch } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-
 import { useDispatch } from 'react-redux';
-import { selectNext, selectPrevious } from './redux_module/todoSlice';
+
+import {
+  selectInside, selectNext, selectOutside, selectPrevious,
+} from './redux_module/todoSlice';
 import useGlobalDOMEvents from './hook';
 
 import Intro from './Page/Intro';
@@ -18,6 +20,8 @@ export default function App() {
       const bindings = {
         ArrowUp: () => dispatch(selectPrevious()),
         ArrowDown: () => dispatch(selectNext()),
+        ArrowLeft: () => dispatch(selectOutside()),
+        ArrowRight: () => dispatch(selectInside()),
       };
 
       (bindings[e.key] ?? notFound)();
