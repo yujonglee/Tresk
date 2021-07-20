8950cc9c5a89ef4c1f554ef7d84969e00e4d543a
yujong-lee <yujonglee@g.skku.edu>
2021-07-20 11:04:21 +0900

    TextField에서 add버튼을 제거하라

--- src/components/TitleField.test.jsx
+++ src/components/TitleField.test.jsx
@@ -9,7 +9,6 @@ import TitleField from './TitleField';
 describe('Input', () => {
   const handleFocus = jest.fn();
   const handleChange = jest.fn();
-  const handleClick = jest.fn();
   const handleKeyDown = jest.fn();
 
   beforeEach(() => {
@@ -23,7 +22,6 @@ describe('Input', () => {
         fieldName="할 일"
         handleFocus={handleFocus}
         handleChange={handleChange}
-        handleClick={handleClick}
         handleKeyDown={handleKeyDown}
       />
     ))
@@ -45,13 +43,5 @@ describe('Input', () => {
     fireEvent.keyDown(inputControl, { key: 'Enter', code: 'Enter' });
 
     expect(handleKeyDown).toBeCalled();
-  });
-
-  it('renders "추가" button listening click event', () => {
-    const { getByRole } = renderTitleField();
-
-    fireEvent.click(getByRole('button', { name: 'add' }));
-
-    expect(handleClick).toBeCalled();
   });
 });
