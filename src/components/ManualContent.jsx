465e9c4ab5c3031e5a8f2d6aa4741f7c89ded308
yujong-lee <yujonglee@g.skku.edu>
2021-07-21 19:40:19 +0900

    add버튼에 대한 도움말을 작성하고, 레이아웃을 조정하라

--- src/components/ManualContent.jsx
+++ src/components/ManualContent.jsx
@@ -1,11 +1,16 @@
-import { BottomNavigation, BottomNavigationAction, Typography } from '@material-ui/core';
+import { useState } from 'react';
 import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Typography,
+} from '@material-ui/core';
+import {
+  AddRounded,
+  UndoRounded,
   BookRounded,
   RotateLeftRounded,
-  UndoRounded,
   HelpOutline,
 } from '@material-ui/icons';
-import { useState } from 'react';
 
 export default function ManualContent() {
   const [value, setValue] = useState('help');
@@ -15,12 +20,24 @@ export default function ManualContent() {
   };
 
   const contents = {
+    add: (
+      <Typography>
+        <br />
+        입력한 할 일을 선택된 항목 아래로 추가해요.
+        <br />
+        선택된 항목은 빨간색으로 표시돼고,
+        <br />
+        클릭 또는 상하좌우 키로 변경할 수 있어요.
+      </Typography>
+    ),
     restore: (
       <Typography>
+        <br />
         <br />
         최근에 완료한 할 일부터 하나씩 복구해요.
         <br />
         원래 위치로 그대로 돌아간답니다.
+        <br />
       </Typography>
     ),
     logbook: (
@@ -28,24 +45,29 @@ export default function ManualContent() {
         <br />
         완료한 할 일들이 시간 순서대로 모여있어요.
         <br />
-        할 일 앞에는 # 이 붙어있는데, 많을수록 더 하위에 있던 할 일이에요.
+        할 일 앞에는 # 이 붙어있는데,
+        <br />
+        많을수록 더 하위에 있던 할 일이에요.
       </Typography>
     ),
     reset: (
       <Typography>
         <br />
+        <br />
         완료한 할 일 목록을 초기화해요.
         <br />
         이후에는 복구할 수 없어요.
+        <br />
       </Typography>
     ),
     help: (
       <Typography>
-        Tresk는 무한 깊이로 할 일을 관리할 수 있는 앱이에요.
         <br />
-        할 일은 언제나 빨간색으로 선택된 항목 아래로 추가된답니다.
+        Tresk는 무한 깊이로 확장되는 To-do 앱이에요.
         <br />
-        각 항목을 클릭하거나 키보드 상하좌우키를 이용해 변경할 수 있어요.
+        자연에서 관찰되는 자기 유사성에서 영감을 받았어요.
+        <br />
+        첫 화면의 나무도 프랙탈 구조를 이용한 것이랍니다.
       </Typography>
     ),
   };
@@ -53,12 +75,15 @@ export default function ManualContent() {
   return (
     <>
       <BottomNavigation value={value} onChange={handleChange}>
+        <BottomNavigationAction label="Add" value="add" icon={<AddRounded />} />
         <BottomNavigationAction label="Restore" value="restore" icon={<UndoRounded />} />
         <BottomNavigationAction label="LogBook" value="logbook" icon={<BookRounded />} />
         <BottomNavigationAction label="Reset" value="reset" icon={<RotateLeftRounded />} />
         <BottomNavigationAction label="Help" value="help" icon={<HelpOutline />} />
       </BottomNavigation>
-      {contents[value]}
+      <div style={{ textAlign: 'center' }}>
+        {contents[value]}
+      </div>
     </>
   );
 }
