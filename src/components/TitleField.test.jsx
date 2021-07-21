b67958b056123daaa0588fa44ae3b4b1976c8b96
yujonglee <61503739+yujong-lee@users.noreply.github.com>
2021-07-21 19:21:30 +0900

    Revert "4주차. 테스크 추가 버튼 삭제"

--- src/components/TitleField.test.jsx
+++ src/components/TitleField.test.jsx
@@ -9,6 +9,7 @@ import TitleField from './TitleField';
 describe('Input', () => {
   const handleFocus = jest.fn();
   const handleChange = jest.fn();
+  const handleClick = jest.fn();
   const handleKeyDown = jest.fn();
 
   beforeEach(() => {
@@ -22,6 +23,7 @@ describe('Input', () => {
         fieldName="할 일"
         handleFocus={handleFocus}
         handleChange={handleChange}
+        handleClick={handleClick}
         handleKeyDown={handleKeyDown}
       />
     ))
@@ -43,5 +45,13 @@ describe('Input', () => {
     fireEvent.keyDown(inputControl, { key: 'Enter', code: 'Enter' });
 
     expect(handleKeyDown).toBeCalled();
+  });
+
+  it('renders "추가" button listening click event', () => {
+    const { getByRole } = renderTitleField();
+
+    fireEvent.click(getByRole('button', { name: 'add' }));
+
+    expect(handleClick).toBeCalled();
   });
 });
